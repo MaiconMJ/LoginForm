f65e244516106e750aeebd21ed8b73463b0ff99b
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-04-24 12:56:24 -0300

    Adionando o esquele em HTML da página

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
 
 function App() {
-  return <h1>APP</h1>
+  return (
+    <body>
+      <div class="caixa__login">
+        <h2>Administrativo</h2>
+        <form>
+          <div class="caixa__login-input">
+            <input type="text" required />
+            <label>Email</label>
+          </div>
+          <div class="caixa__login-input">
+            <input type="password" required />
+            <label>Senha</label>
+          </div>
+          <a>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Acessar
+          </a>
+        </form>
+      </div>
+    </body>
+  )
 }
 
 export default App;
