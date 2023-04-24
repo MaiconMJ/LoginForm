c3ccdbda85a57f49b38510106a2b36a465fac63a
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-04-24 13:35:32 -0300

    Adicionado o Router

---
+++ src/router/Router.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login"; 
+import SignUp from "../pages/SignUp"
+import { Erro } from '../Pages/Erro';
+
+const Router = () => {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Login />} />
+          <Route path={"/signup"} element={<SignUp />} />
+          <Route path={"*"} element={<Erro />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default Router;
