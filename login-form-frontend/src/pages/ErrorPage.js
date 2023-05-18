02aa43cbc01291372c47a37e592278557f2a3780
Maicon Miranda de Jesus <maicon.miranda77@hotmail.com>
2023-05-18 12:56:17 -0300

    error page finalizada

--- login-form-frontend/src/pages/ErrorPage.js
+++ login-form-frontend/src/pages/ErrorPage.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
 
 const ErrorPage = () => {
+
+  const navigate = useNavigate()
+
   return (
     <body>
       <div class="caixa__login">
         <h2>Page not found</h2>
         <form style={{textAlign: 'center'}}>
-          <a >
+          <a href='/'>
             <span></span>
             <span></span>
             <span></span>
             <span></span>
-            Back to Login!
+            Back to Login
           </a>
         </form>
       </div>
